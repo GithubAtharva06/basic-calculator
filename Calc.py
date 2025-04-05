f735a4eda62114049cb412bb3a0c16d2e7942dd0
GithubAtharva06 <atharvachavan206@gmail.com>
2025-04-05 16:36:31 +0530

    Create Calc.py

---
+++ Calc.py
@@ -0,0 +1,16 @@
+a= int(input("enter a number"))
+b= int(input("enter 2nd number"))
+add = a+b
+print("the addition of a and b is", add)
+sub = a-b
+if a<b:
+    print("invalid number")
+else:
+    print("the subtraction of a and b is", sub)
+multiply = a*b
+print("the multiplication of and and b is", multiply)
+div = a/b
+if a<b:
+    print("invalid number")
+else:
+    print("the division of a and b is", div)
